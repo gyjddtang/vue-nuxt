9d33c03d721300de584a522ae57fb6c59572c548
yj02 <gaoyuanjie@gznb.com>
2017-10-25 15:59:37 +0800

    Initial commit

---
+++ pages/test.vue
@@ -0,0 +1,30 @@
+<!--
+  - Created by Mili on 2017/10/25
+ -->
+
+<template lang="html">
+  <section class="container">
+    <div>{{ testMsg }}</div>
+  </section>
+</template>
+
+<script>
+  export default {
+    name: 'title',
+    data () {
+      return {
+        testMsg: '我是测试的数据'
+      }
+    }
+  }
+</script>
+
+<style>
+  .container {
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
+</style>
